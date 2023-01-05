61c13cb86808958ed4f04d8e438eaa520949cfe1
teacher_cheater <johnnykayfun@gmail.com>
2023-01-05 21:48:57 +0300

    fix bugs, add pizza card

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,15 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Categories() {
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const showActiveIndex = (index) => {
+    setActiveIndex(index);
+  };
+
   return (
     <div className="categories">
       <ul>
-        <li className="active">Все</li>
-        <li>Мясные</li>
-        <li>Вегетарианская</li>
-        <li>Гриль</li>
-        <li>Острые</li>
-        <li>Закрытые</li>
+        <li
+          onClick={() => showActiveIndex(0)}
+          className={activeIndex === 0 ? "active" : ""}
+        >
+          Все
+        </li>
+        <li
+          onClick={() => showActiveIndex(1)}
+          className={activeIndex === 1 ? "active" : ""}
+        >
+          Мясные
+        </li>
+        <li
+          onClick={() => showActiveIndex(2)}
+          className={activeIndex === 2 ? "active" : ""}
+        >
+          Вегетарианская
+        </li>
+        <li
+          onClick={() => showActiveIndex(3)}
+          className={activeIndex === 3 ? "active" : ""}
+        >
+          Гриль
+        </li>
+        <li
+          onClick={() => showActiveIndex(4)}
+          className={activeIndex === 4 ? "active" : ""}
+        >
+          Острые
+        </li>
+        <li
+          onClick={() => showActiveIndex(5)}
+          className={activeIndex === 5 ? "active" : ""}
+        >
+          Закрытые
+        </li>
       </ul>
     </div>
   );
